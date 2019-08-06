e697cc45b9ece2c15e275cc2a555466eb89b54af
Mr-Hamster <Nazar9910@ukr.net>
2019-08-07 00:49:55 +0300

    fromHome

--- src/screens/HistoricalValues.js
+++ src/screens/HistoricalValues.js
@@ -24,15 +24,13 @@ export default class HistoricalValues extends React.Component{
         const {value, value_classificaton} = this.state
         return(
             <div className="wrapper">
-                <h2>
-                    Historical Values
-                </h2>
-                <span>
-                    Now
-                </span>
-                
-                {value_classificaton} : {value}
-                
+                {value_classificaton}
+                <div className="scale">
+                    <div className="target" style={{ bottom: `${value*2}px` }}/>
+                    <div style={{ position:'absolute', bottom: `${value*2}px`, left:'60px', transition: 'transform .2s ease-in-out', }}>
+                        {value}
+                    </div>
+                </div>
             </div>
         );
     }
