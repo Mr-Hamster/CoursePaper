9d17575143bac406fc9fab39f6b036dbee7ceac6
Mr-Hamster <Nazar9910@ukr.net>
2019-08-14 00:28:58 +0300

    DSC-11

--- src/screens/ChangeStatistic.js
+++ src/screens/ChangeStatistic.js
@@ -3,73 +3,61 @@ import '../styles/ChangeStatistic.scss'
 import * as api from '../api/index';
 import { Table } from 'react-bootstrap'
 
-let currentPrice;
+let arrRes = [];
 
 export default class ChangeStatistic extends React.Component{
     state = {
-        currentPrice: 0
+        arr: [],
     }
 
     componentDidMount(){
         let currentTime = new Date().getTime();
-        let times;
+        let times, histo;
+        const { from, to } = this.props;
 
-        this.getCurrentPrice();
+        api.crudBuilder(`https://min-api.cryptocompare.com/data/histominute?fsym=${from}&tsym=${to}&limit=1`).get().then(
+            resp => {
+                let currentPrice = resp.data.Data[1].close;
+                this.getStatistic(histo = 'minute', times = 5, currentPrice);
+                this.getStatistic(histo = 'minute', times = 15, currentPrice);
+                this.getStatistic(histo = 'hour', times = 2, currentPrice);
+                this.getStatistic(histo = 'hour', times = 5, currentPrice);
+                this.getStatistic(histo = 'hour', times = 13, currentPrice);
+                this.getStatistic(histo = 'day', times = 2, currentPrice);
+                this.getStatistic(histo = 'day', times = 8, currentPrice);
+                this.getStatistic(histo = 'day', times = 31, currentPrice);
+            }).catch(err => {
+                alert(`${err}`);
+            });
 
-        // let timestamp_5min = currentTime - 300;
-        // let timestamp_15min = currentTime - 900;
-        // let timestamp_1hour = currentTime - 3600;
-        // let timestamp_4hour = currentTime - 14400;
-        // let timestamp_12hour = currentTime - 43200;
-        // let timestamp_1day = currentTime - 86400;
-        // let timestamp_7days = currentTime - 604800;
-        // let timestamp_30days = currentTime - 18144000;
-        // this.getStatistic(timestamp_5min, times = 1);
-        // this.getStatistic(timestamp_15min, times = 3);
-        // this.getStatistic(timestamp_1hour, times = 12);
-        // this.getStatistic(timestamp_4hour, times = 48);
-        // this.getStatistic(timestamp_12hour, times = 144);
-        // this.getStatistic(timestamp_1day, times = 288);
-        this.getStatistic(times = 7);
-        this.getStatistic(times = 30);
-        this.getStatistic(times = 10);
-        // this.getStatistic(timestamp_30days, times = 8640);
     }
 
-    getStatistic = (times) => {
+    getStatistic = (histo, times, currentPrice) => {
         const { from, to } = this.props;
         let value;
         
-        api.crudBuilder(`https://min-api.cryptocompare.com/data/histoday?fsym=${from}&tsym=${to}`).get().then(
+        api.crudBuilder(`https://min-api.cryptocompare.com/data/histo${histo}?fsym=${from}&tsym=${to}&limit=40`).get().then(
             resp => {
-                console.log(resp.data.Data)
                 value = resp.data.Data[resp.data.Data.length - times].close;
-                console.log('asdasd',resp.data.Data[resp.data.Data.length - times].time, currentPrice );
-                console.log('res: ', this.getPercent(currentPrice, value) + '%');
+                let res =  this.getPercent(currentPrice, value);
+                arrRes.push(res);
+                this.setState({
+                    arr: arrRes
+                })
             }).catch(err => {
-                alert(`${err}`);
+                console.log(err);
             });
     }
 
     getPercent = (currentPrice, value) => {
         let result = value * 100 / currentPrice;
         result = 100 - result;
-        return -1*result.toFixed(3);
-    }
-
-    getCurrentPrice = () => {
-        const { from, to } = this.props;
-
-        api.crudBuilder(`https://min-api.cryptocompare.com/data/histominute?fsym=${from}&tsym=${to}&limit=1`).get().then(
-            resp => {
-                currentPrice = resp.data.Data[1].close;
-                console.log('currentPrice', currentPrice)
-            }).catch(err => {
-                alert(`${err}`);
-            });
+        return -1*result.toFixed(3)+'%';
     }
 
     render(){
+        const { arr } = this.state; 
+        console.log(this.state);
         return(
             <Table striped bordered hover variant="dark" style={{width: "70%", marginTop: '20px'}}>
                     <thead>
@@ -86,14 +74,14 @@ export default class ChangeStatistic extends React.Component{
                     </thead>
                     <tbody>
                         <tr>
-                            <th>0</th>
-                            <th>1</th>
-                            <th>2</th>
-                            <th>3</th>
-                            <th>0</th>
-                            <th>1</th>
-                            <th>2</th>
-                            <th>3</th>
+                            <th>{arr[0]}</th>
+                            <th>{arr[1]}</th>
+                            <th>{arr[2]}</th>
+                            <th>{arr[3]}</th>
+                            <th>{arr[4]}</th>
+                            <th>{arr[5]}</th>
+                            <th>{arr[6]}</th>
+                            <th>{arr[7]}</th>
                         </tr>  
                         <tr>
                             <th>0</th>
