af3dc2086ecc55180204598858f89aafb8dde9c7
uniway <user@Users-MacBook-4.local>
2019-09-11 02:15:45 -0600

    toFixed(3) number

--- src/screens/TableFavouriteCoins.js
+++ src/screens/TableFavouriteCoins.js
@@ -45,10 +45,10 @@ export default class TableFavouriteCoins extends React.Component{
                                         </div>
                                     </td>
                                     <td>{item.symbol.toUpperCase()}</td>
-                                    <td>{item.market_data.current_price.usd}$</td>
-                                    <td style={{color: item.market_data.price_change_percentage_1h_in_currency.usd > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_1h_in_currency.usd}%</td>
-                                    <td style={{color: item.market_data.price_change_percentage_24h > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_24h}%</td>      
-                                    <td style={{color: item.market_data.price_change_percentage_7d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_7d}%</td>                               
+                                    <td>{item.market_data.current_price.usd.toFixed(3)}$</td>
+                                    <td style={{color: item.market_data.price_change_percentage_1h_in_currency.usd > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_1h_in_currency.usd.toFixed(3)}%</td>
+                                    <td style={{color: item.market_data.price_change_percentage_24h > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_24h.toFixed(3)}%</td>      
+                                    <td style={{color: item.market_data.price_change_percentage_7d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_7d.toFixed(3)}%</td>                               
                                     <td>{item.market_data.total_volume.usd}$</td>
                                     <td>{item.market_data.circulating_supply}</td>
                                     <td>{item.market_data.market_cap.usd}$</td>
@@ -89,14 +89,14 @@ export default class TableFavouriteCoins extends React.Component{
                                     </td>
                                     <td>{item.symbol.toUpperCase()}</td>
                                     <td>{item.market_data.current_price.usd}$</td>
-                                    <td style={{color: item.market_data.price_change_percentage_1h_in_currency.usd > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_1h_in_currency.usd}%</td>
-                                    <td style={{color: item.market_data.price_change_percentage_24h > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_24h}%</td>      
-                                    <td style={{color: item.market_data.price_change_percentage_7d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_7d}%</td>                               
-                                    <td style={{color: item.market_data.price_change_percentage_14d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_14d}%</td>  
-                                    <td style={{color: item.market_data.price_change_percentage_30d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_30d}%</td>  
-                                    <td style={{color: item.market_data.price_change_percentage_60d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_60d}%</td>  
-                                    <td style={{color: item.market_data.price_change_percentage_200d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_200d}%</td>  
-                                    <td style={{color: item.market_data.price_change_percentage_1y > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_1y}%</td>  
+                                    <td style={{color: item.market_data.price_change_percentage_1h_in_currency.usd > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_1h_in_currency.usd.toFixed(3)}%</td>
+                                    <td style={{color: item.market_data.price_change_percentage_24h > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_24h.toFixed(3)}%</td>      
+                                    <td style={{color: item.market_data.price_change_percentage_7d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_7d.toFixed(3)}%</td>                               
+                                    <td style={{color: item.market_data.price_change_percentage_14d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_14d.toFixed(3)}%</td>  
+                                    <td style={{color: item.market_data.price_change_percentage_30d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_30d.toFixed(3)}%</td>  
+                                    <td style={{color: item.market_data.price_change_percentage_60d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_60d.toFixed(3)}%</td>  
+                                    <td style={{color: item.market_data.price_change_percentage_200d > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_200d.toFixed(3)}%</td>  
+                                    <td style={{color: item.market_data.price_change_percentage_1y > 0 ? 'green' : 'red' }} >{item.market_data.price_change_percentage_1y.toFixed(3)}%</td>  
 
                                 </tr>
                             ))
