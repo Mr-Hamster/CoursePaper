e6740d7faf7320978a05a704a933f77a221475a9
Nazarii Khomiak <nazarii.k@volpis.com>
2021-05-18 10:08:05 +0300

    .

--- server/src/exceptions/UserNotVerifiedException.ts
+++ server/src/exceptions/UserNotVerifiedException.ts
@@ -2,7 +2,7 @@ import HttpException from './HttpException';
 
 class UserNotVerifiedException extends HttpException {
   constructor() {
-    super(401, 'User is not verify!');
+    super(302, 'User is not verify!');
   }
 }
 
